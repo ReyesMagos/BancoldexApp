703706e16f37f0d4c84191bfba1875185734fd2d
alexis0592 <yefry.calderon@udea.edu.co>
2014-08-30 15:49:10 -0500

    Strings

---
+++ Bancoldex/src/com/reyesmagos/bancoldex/bancoldexapp/activities/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.reyesmagos.bancoldex.bancoldexapp.activities.register;
+
+import com.reyesmagos.bancoldex.bancoldexapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_activity);
+	}
+
+}
