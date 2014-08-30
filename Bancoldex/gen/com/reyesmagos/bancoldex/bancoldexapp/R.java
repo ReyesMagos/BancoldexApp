bc32d129c711dd01425704de2cd7e428b3aa7ccd
reyesmagos <reyemagossoft.co@gmail.com>
2014-08-30 14:35:20 -0500

    string

--- Bancoldex/gen/com/reyesmagos/bancoldex/bancoldexapp/R.java
+++ Bancoldex/gen/com/reyesmagos/bancoldex/bancoldexapp/R.java
@@ -25,8 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int UsernameTextView=0x7f080000;
         public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login_activity_main=0x7f030000;
@@ -38,6 +38,13 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        /**  controller 
+         */
+        public static final int ok_label=0x7f050003;
+        public static final int password_label=0x7f050005;
+        /**  login 
+         */
+        public static final int username_label=0x7f050004;
     }
     public static final class style {
         /** 
